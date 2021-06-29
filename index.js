85985747a9469ac94d78577a32908d2d984fda47
tin cong phan <tincongphan@gmail.com>
2021-06-29 17:06:49 +0700

    Hosting

--- index.js
+++ index.js
@@ -17,24 +17,36 @@
 
 //  ====> Var &  Let & const
 
-var number1 = 6;
-if(true){
-    var number1 = 8;
-}
-console.log(number1); // 8
+// var number1 = 6;
+// if(true){
+//     var number1 = 8;
+// }
+// console.log(number1); // 8
 
 
-let number2 = 6;
-if(true){
-    let number2 = 8;
-}
-console.log(number2); // 6
+// let number2 = 6;
+// if(true){
+//     let number2 = 8;
+// }
+// console.log(number2); // 6
 
 
-const number3 = 6;
-number3 = 8; // error
+// const number3 = 6;
+// number3 = 8; // error
 
-const number4 = {id : 1, name: "abc" }
-number4 = {id: 2, name: 'momo'} // error
-number4.name = 'xyz';
-console.log(number4);  // {id: 1, name: "xyz"}
+// const number4 = {id : 1, name: "abc" }
+// number4 = {id: 2, name: 'momo'} // error
+// number4.name = 'xyz';
+// console.log(number4);  // {id: 1, name: "xyz"}
+
+
+// ======> Hoisting
+
+//  Khi khai báo với var thì biến sẽ được kéo lên đầu, còn let và const thì không.
+console.log(a); // undefinded
+var a = 1;
+
+console.log(b);
+let b = 1; // error
+
+//  let và const khắc phục hoisting của var. Nghĩa là không thể sử dụng let & const trước khi khai báo.
