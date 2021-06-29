f55d731e4ce72d6d7bd069f0703dac9511ef9848
tin cong phan <tincongphan@gmail.com>
2021-06-29 16:18:54 +0700

    Var - Let - Const

--- index.js
+++ index.js
@@ -13,3 +13,28 @@
   10/   OOP
   11/   Fillter, Find, FindIndex, ForEach, Map, Reduce, Reverse, Sort.
  */
+
+
+//  ====> Var &  Let & const
+
+var number1 = 6;
+if(true){
+    var number1 = 8;
+}
+console.log(number1); // 8
+
+
+let number2 = 6;
+if(true){
+    let number2 = 8;
+}
+console.log(number2); // 6
+
+
+const number3 = 6;
+number3 = 8; // error
+
+const number4 = {id : 1, name: "abc" }
+number4 = {id: 2, name: 'momo'} // error
+number4.name = 'xyz';
+console.log(number4);  // {id: 1, name: "xyz"}
