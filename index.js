8bf26b4e6f0cd0b6c857420b80a479702b1b3334
tin cong phan <tincongphan@gmail.com>
2021-06-29 16:03:43 +0700

    New Feature's ES6

---
+++ index.js
@@ -0,0 +1,15 @@
+
+/*      =======> New Feature's ES6
+
+  1/    Let & const
+  2/    Arrow function
+  3/    Import & export
+  4/    Restparameter & spread oparater
+  5/    Default parameter
+  6/    Destructuring
+  7/    Template strings
+  8/    Objects literal
+  9/    For in & For of
+  10/   OOP
+  11/   Fillter, Find, FindIndex, ForEach, Map, Reduce, Reverse, Sort.
+ */
